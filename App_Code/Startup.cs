45811401c80bd328465e7916162e0f0ff538dd92
shahzadiqbal <shahzadiqbal.may18@gmail.com>
2017-04-04 06:41:20 +0500

    done

---
+++ App_Code/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Fyp.Startup))]
+
+
+/// <summary>
+/// Summary description for Startup
+/// </summary>
+/// 
+
+namespace Fyp
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+
+
+    }
+}
