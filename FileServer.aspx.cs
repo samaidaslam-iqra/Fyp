ac6f7f8d1d1ee59b1f6830637de7978219f517a2
samaidaslam-iqra <samaid_92@hotmail.com>
2017-07-06 21:38:01 +0500

    size to Mb

--- FileServer.aspx.cs
+++ FileServer.aspx.cs
@@ -77,7 +77,8 @@ public partial class FileServer : System.Web.UI.Page
         foreach (string strfile in Directory.GetFiles(Server.MapPath(url)))
         {
             FileInfo fi = new FileInfo(strfile);
-            dt.Rows.Add(fi.Name, fi.Length.ToString(), GetFileTypeByExtension(fi.Extension));
+            double ss = Convert.ToDouble(fi.Length) / 1048576;
+            dt.Rows.Add(fi.Name, Math.Round(ss,2).ToString()+" MB", GetFileTypeByExtension(fi.Extension));
 
         } GridView1.DataSource = dt;
         GridView1.DataBind();
